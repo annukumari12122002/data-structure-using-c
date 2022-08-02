7c101466db2fc1a0f496c6488cbc166d933952be
annukumari12122002 <annukumari997392@gmail.com>
2022-08-02 00:56:35 -0700

    02-08-2022_strlen function push

---
+++ strlen-code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+int length;
+printf("enter a string to calculate its length\n");
+gets(a);
+length=strlen(a);
+printf("length of the string=%d\n",length);
+return 0;
+}
